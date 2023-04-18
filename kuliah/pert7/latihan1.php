045f0caf1ed13f69f52b7edf079e3b766207f7bc
rizki270703 <rizkinurhidayat013@gmail.com>
2023-04-19 04:21:21 +0700

    menambahkan 3 database di pert7

---
+++ kuliah/pert7/latihan1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <h3>Daftar Mahasiswa</h3>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>#Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/rizki.jpg"></td>
+      <td>0897</td>
+      <td>Rizki Nurhidayat</td>
+      <td>rizkinurhidayat<@gmail.com/td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+      </tr>
+
+
+    </table>
+  
+</body>
+</html>
